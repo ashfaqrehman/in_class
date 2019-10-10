23a8366b04da2cb4f9613df5cdce91beb313df0c
Ashfaq Rehman <ashfaq.rehman@watchguard.com>
2019-10-09 21:37:23 -0400

    slug and author

--- blog/admin.py
+++ blog/admin.py
@@ -5,12 +5,19 @@ from . import models
 
 class PostAdmin(admin.ModelAdmin):
     list_display = (
+        'author',
         'title',
         'created',
         'updated',
     )
     search_fields = (
         'title',
+        'author__username',
+        'author__first_name',
+        'author__last_name',
     )
+    prepopulated_fields = {
+        'slug':('title',)
+    }
 
 admin.site.register(models.Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,48 @@
+from django.conf import settings
 from django.db import models
+
 
 class Post(models.Model):
     """
     Stores posts for the blog
     """
+    DRAFT = 'draft'
+    PUBLISHED = 'published'
+    STATUS_CHOICES = [
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published'),
+
+    ]
+
+
+
     title = models.CharField(max_length=255,help_text='The test of the content')
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=DRAFT,
+        help_text='Set to "Published" to make pubblic'
+    )
+    slug = models.SlugField(
+        null=True,
+        unique_for_date='published',
+    )
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text = 'The publishing date'
+    )
+    
+    
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.PROTECT,
+        related_name = 'blog_posts',
+        null = True,
+    )
 
     class Meta:
         ordering = ['-created']
