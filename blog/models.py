493b8132666559f0122de61b253a4a97ea14c8d6
Ashfaq Rehman <ashfaq.rehman@watchguard.com>
2019-10-09 20:42:14 -0400

    add meta

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,14 @@ class Post(models.Model):
     """
     Stores posts for the blog
     """
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255,help_text='The test of the content')
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+    
