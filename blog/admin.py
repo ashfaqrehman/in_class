2c9de6cd4c99a20a47de05010b974aa7cb7cc96a
Ashfaq Rehman <ashfaq.rehman@watchguard.com>
2019-10-16 21:50:47 -0400

    relationships

--- blog/admin.py
+++ blog/admin.py
@@ -3,12 +3,29 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    
+
+    prepopulated_fields = {'slug': ('name',)}
+
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'author',
         'title',
         'created',
         'updated',
+    )
+
+    list_filter = (
+        'status',
+        'topics',
     )
     search_fields = (
         'title',
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,19 @@
 from django.conf import settings
 from django.db import models
 
+class Topic(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-name']
 
 class Post(models.Model):
     """
@@ -27,6 +40,12 @@ class Post(models.Model):
         null=False,
         unique_for_date='published',
     )
+    
+    topics = models.ManyToManyField(
+        Topic,
+        related_name='blog_posts'
+    )
+    
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
